a00faf7d2dc48c8d49def3ba22a0c04028df1ac7
sindhujamohan9 <sindhujamohan92@gmail.com>
2023-05-21 09:36:12 -0600

    Create import_libraries_and_datasets.py

---
+++ EDA_Pandas/import_libraries_and_datasets.py
@@ -0,0 +1,23 @@
+print('ways to import datasets')
+
+# mc12 = pd.read_csv('/content/Two+Machines.csv')
+
+# # in-built datasets in seaborn
+
+# sns.get_dataset_names()
+# sns.load_dataset('tips')
+
+# # in-built datasets in seaborn
+
+# sns.get_dataset_names()
+# sns.load_dataset('tips')
+
+# # Using Pydatasets
+
+# housing = data('Housing')
+# housing = data('diamonds')
+
+
+housing = data('Housing')
+
+housing
