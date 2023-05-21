1df19f1f8f7953e5f200c610a695879f67be4764
sindhujamohan9 <sindhujamohan92@gmail.com>
2023-05-21 09:48:41 -0600

    Update line_plots.py

--- EDA_Pandas/line_plots.py
+++ EDA_Pandas/line_plots.py
@@ -6,23 +6,6 @@ from pydataset import data
 
 print('ways to import datasets')
 
-# mc12 = pd.read_csv('/content/Two+Machines.csv')
-
-# # in-built datasets in seaborn
-
-# sns.get_dataset_names()
-# sns.load_dataset('tips')
-
-# # in-built datasets in seaborn
-
-# sns.get_dataset_names()
-# sns.load_dataset('tips')
-
-# # Using Pydatasets
-
-# housing = data('Housing')
-# housing = data('diamonds')
-
 housing = data('Housing')
 
 housing.plot()
