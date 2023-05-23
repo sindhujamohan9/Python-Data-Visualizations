317028d4504beef1a38f71a05ab20094d1e57fef
sindhujamohan9 <sindhujamohan92@gmail.com>
2023-05-23 14:32:51 -0600

    Update subplots.py

--- EDA_matplotlib/subplots.py
+++ EDA_matplotlib/subplots.py
@@ -60,3 +60,42 @@ ax2.set_ylabel('% change')
 
 fig.suptitle('Australian Population')
 ax2.annotate('What happened here?', xy=(min_pct_change_x, min_pct_change_y), xytext=(min_pct_change_x-8, min_pct_change_y*1.2), arrowprops={})
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+
+# First the bar plot
+ax1.bar(story_counts.index, story_counts)
+ax1.set_xticks(story_counts.index) # see what happens without this
+ax1.set_title('Number of stories')
+ax1.set_xlabel('Stories')
+ax1.set_ylabel('Number of houses')
+ax1.grid(axis='y', alpha=0.5, ls=':');
+
+# Pie chart on the right
+ax2.pie(story_counts, labels=story_counts.index, autopct='%f') # Try replacing %f with %f%%, then %.f%%, then %.1f%%
+ax2.legend()
+ax2.set_title('Portions of houses with various stories');
+
+fig.tight_layout()
+
+fig, ax = plt.subplots(2, 2, figsize=(10, 10))
+
+# ax is an object with four axes 0,0 - top left plot, 0,1 - top right plot, so on
+
+ax[0,0].hist(housing_df['price'], bins=10)
+ax[0,0].set_ylabel('Price')
+
+ax[0,1].scatter(housing_df['lotsize'], housing_df['price'])
+
+ax[1,0].scatter(housing_df['price'], housing_df['lotsize'])
+ax[1,0].set_xlabel('Price')
+ax[1,0].set_ylabel('Lot Size')
+
+ax[1,1].hist(housing_df['lotsize'], bins=10)
+ax[1,1].set_xlabel('Lot Size')
+
+fig.suptitle('Lotsize and Price Correlogram')
+fig.tight_layout(rect=[0, 0, 1, 0.95]);
+
+
+
