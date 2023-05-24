cf8d1b8cc8949af53aa54fc4e3199f6746efc818
sindhujamohan9 <sindhujamohan92@gmail.com>
2023-05-24 12:04:33 -0600

    Create strip_and_swarm_plots.py

---
+++ EDA_Seaborn/strip_and_swarm_plots.py
@@ -0,0 +1,18 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+from pydataset import data 
+
+housing_df.head()
+
+housing_df.head()
+
+sns.catplot(housing_df, y='price', hue='bedrooms')
+
+sns.catplot(housing_df, y='price')
+
+sns.catplot(housing_df, x='bedrooms', y='price', hue='driveway', dodge=True); # strip plot
+
+sns.catplot(housing_df, x='bedrooms', y='price', hue='driveway', kind='swarm');
+
+sns.catplot(housing_df, x='bedrooms', y='price', hue='driveway', kind='swarm', dodge=True, aspect=2);
