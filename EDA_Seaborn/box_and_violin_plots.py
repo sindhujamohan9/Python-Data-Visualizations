cd247a6b9c6f850ce764b91ab88e8c8627ef2d95
sindhujamohan9 <sindhujamohan92@gmail.com>
2023-05-24 12:05:12 -0600

    Create box_and_violin_plots.py

---
+++ EDA_Seaborn/box_and_violin_plots.py
@@ -0,0 +1,14 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+from pydataset import data 
+
+housing_df.head()
+
+sns.catplot(housing_df, x='bedrooms', y='price', kind='box')
+
+sns.catplot(housing_df, x='bedrooms', y='price', kind='violin')
+
+sns.catplot(housing_df, x='bedrooms', y='price', kind='box', hue='driveway')
+
+sns.catplot(housing_df, x='bedrooms', y='price', kind='violin', hue='driveway', split=True)
